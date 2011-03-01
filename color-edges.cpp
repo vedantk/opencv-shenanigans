d6ee93dcf6473e1d3604ec9de25a9fd39a39a8c2
Vedant Kumar <vk@vedantk.com>
2011-03-01 18:59:37 -0500

    Heat display, using Vec2i

--- color-edges.cpp
+++ color-edges.cpp
@@ -2,6 +2,7 @@
 #include <highgui.h>
 using namespace cv;
 
+#include <cmath>
 #include <cstdlib>
 #include <iostream>
 using namespace std;
@@ -19,14 +20,14 @@ void matStats(Mat& mat)
 	cin.get();
 }
 
-void getRegion(Mat& mat, bool (*lambda)(u8 shade), u8 markAs,
-			   vector<int>& ivec, vector<int>& jvec)
+void getRegion(Mat& mat, u8 markAs,
+			   bool (*lambda)(u8 shade), vector<Vec2i>& posns)
 {
 	int visited = 0;
-	int to_visit = ivec.size();
+	int to_visit = posns.size();
 	while (to_visit > visited) {
-		int ci = ivec[visited];
-		int cj = jvec[visited];
+		Vec2i cur = posns[visited];
+		int ci = cur[0], cj = cur[1];
 		++visited;
 
 		for (int dx=-1; dx < 2; ++dx) {
@@ -37,9 +38,8 @@ void getRegion(Mat& mat, bool (*lambda)(u8 shade), u8 markAs,
 					&& idx != ci && idy != cj
 					&& lambda(mat.at<u8>(idx, idy)))
 				{
-					ivec.push_back(idx);
-					jvec.push_back(idy);
 					mat.at<u8>(idx, idy) = markAs;
+					posns.push_back(Vec2i(idx, idy));
 					++to_visit;
 				}
 			}
@@ -49,7 +49,7 @@ void getRegion(Mat& mat, bool (*lambda)(u8 shade), u8 markAs,
 
 int randRange(int a, int b)
 {
-	return (rand() % (b + 1)) + a;
+	return a + (abs(rand()) % (b - a));
 }
 
 bool shadeFilter(u8 shade)
@@ -59,17 +59,15 @@ bool shadeFilter(u8 shade)
 
 void regionLabel(Mat& edges, Mat& regions)
 {
+	vector<Vec2i> posns;
 	regions = edges.clone();
-	vector<int> ivec, jvec;
 	for (int i=0; i < regions.rows; ++i) {
 		for (int j=0; j < regions.cols; ++j) {
 			if (shadeFilter(regions.at<u8>(i, j))) {
-				ivec.push_back(i);
-				jvec.push_back(j);
-				u8 color = randRange(32, 192);
-				getRegion(regions, shadeFilter, color, ivec, jvec);
-				ivec.clear();
-				jvec.clear();
+				posns.push_back(Vec2i(i, j));
+				u8 color = randRange(128, 192);
+				getRegion(regions, color, shadeFilter, posns);
+				posns.clear();
 			}
 		}
 	}
@@ -83,9 +81,9 @@ void colorize(Mat& mat, Mat& out)
 	}
 	for (int i=0; i < mat.rows; ++i) {
 		for (int j=0; j < mat.cols; ++j) {
-			planes[0].at<u8>(i, j) = (i * j) % 256;
-			planes[1].at<u8>(i, j) ^= 0xFF;
-			planes[2].at<u8>(i, j) = 0xFF - mat.at<u8>(i, j);
+			u8 cur = mat.at<u8>(i, j);
+			planes[0].at<u8>(i, j) = 0xFF - cur;
+			planes[1].at<u8>(i, j) = (cur * cur) % 0xFF;
 		}
 	}
 	merge(planes, out);
@@ -95,15 +93,12 @@ int main(int argc, char** argv)
 {
 	srand(time(NULL));
 	VideoCapture cap(0);
-	if(!cap.isOpened()) {
+	if (!cap.isOpened()) {
 		puts("Couldn't open camera.");
-        return -1;
+		return -1;
 	}
 
-	const char* windows[] = {
-		"Color Input", "Smoothed Grayscale",
-		"Edge Detection", "Regions", "Colorized Regions"
-	};
+	const char* windows[] = {"Feed", "Smoothed", "Regions", "Heat"};
 	for (size_t i=0; i < sizeof(windows)/sizeof(char*); ++i) {
 		namedWindow(windows[i], 1);
 	}
@@ -112,16 +107,15 @@ int main(int argc, char** argv)
 	while (true) {
 		cap >> frame;
 		cvtColor(frame, gray, CV_BGR2GRAY);
-		GaussianBlur(gray, gray, Size(3, 3), 2, 2);
-		Canny(gray, edges, 0, 30, 3, true);
+		GaussianBlur(gray, gray, Size(3, 3), 5, 5);
+		Canny(gray, edges, 0, 50, 3, true);
 		regionLabel(edges, regions);
-		colorize(regions, colorized);
+		colorize(gray, colorized);
 
 		imshow(windows[0], frame);
 		imshow(windows[1], gray);
-		imshow(windows[2], edges);
-		imshow(windows[3], regions);
-		imshow(windows[4], colorized);
+		imshow(windows[2], regions);
+		imshow(windows[3], colorized);
 		if (waitKey(10) >= 0) break;
 	}
 
